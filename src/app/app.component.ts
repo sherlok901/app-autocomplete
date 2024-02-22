64c0ad82f84b0b1715f7f8fda97287299deae59e
Igor <igorbugaenko21@gmail.com>
2024-02-22 21:13:00 +0200

    3rd autocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,15 @@ export class AppComponent {
 
   options1: string[] = ['Apple', 'Banana', 'Orange'];
   options2: string[] = ['Red', 'Green', 'Blue'];
+  options3: string[] = [];
 
   myControl = new FormControl('');
   myControl2 = new FormControl('');
+  myControl3 = new FormControl('');
 
   filteredOptions: Observable<string[]> = of();
   filteredOptions2: Observable<string[]> = of();
+  filteredOptions3: Observable<string[]> = of();
 
   ngOnInit() {
     this.filteredOptions = this.myControl.valueChanges.pipe(
@@ -30,11 +33,25 @@ export class AppComponent {
       startWith(''),
       map(value => this._filter(value, this.options2))
     );
+
+    // Emulate fetching data from an external API
+    this.fetchExternalData().subscribe(data => {
+      this.options3 = data; // Assuming data is an array of strings
+      this.filteredOptions3 = this.myControl3.valueChanges.pipe(
+        startWith(''),
+        map(value => this._filter(value, this.options3))
+      );
+    });
   }
 
   private _filter(value: string | null, options: string[]): string[] {
     const filterValue = value?.toLowerCase() ?? "";
 
     return options.filter(option => option.toLowerCase().includes(filterValue ));
+  }
+
+  // Emulate fetching data from an external API
+  fetchExternalData(): Observable<string[]> {
+    return of(['Apple', 'Banana', 'Orange', 'Pineapple', 'Grapes']);
   }
 }
